c6e8aeee3f44f782562d8dc61cf7e3388ec4a1cb
gfonseca <gfonseca@bunq.com>
2022-11-24 17:29:03 +0100

    Add account_age_in_months column to customers staging table

--- sde_dbt_tutorial/models/staging/stg_eltool__customers.sql
+++ sde_dbt_tutorial/models/staging/stg_eltool__customers.sql
@@ -8,6 +8,8 @@ with source as (
         state_code,
         datetime_created::TIMESTAMP AS datetime_created,
         datetime_updated::TIMESTAMP AS datetime_updated,
+        (DATE_PART('year', now()) - DATE_PART('year', datetime_created::TIMESTAMP)) * 12 +
+              (DATE_PART('month', now()) - DATE_PART('month', datetime_created::TIMESTAMP)) as account_age_in_months,
         dbt_valid_from,
         dbt_valid_to
     from source
