ddad426921e72509ff7a04ebe69874ad47a9ec96
gfonseca <gfonseca@bunq.com>
2022-11-29 19:41:31 +0100

    int_orders__pivoted model created

---
+++ sde_dbt_tutorial/models/order_payment_method_amounts.sql
@@ -0,0 +1,14 @@
+{%- set payment_methods = ["bank_transfer", "credit_card", "gift_card"] -%}
+
+select
+order_id,
+
+{% for payment_method in payment_methods %}
+sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount,
+{% if not loop.last %},{% endif %}
+{% endfor %}
+
+
+sum(amount) as total_amount
+from {{ ref('raw_payments') }}
+group by 1
