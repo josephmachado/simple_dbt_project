492ff6c0fdcb720ee1a2994a3c43b0a739aa4bf4
gfonseca <gfonseca@bunq.com>
2022-11-29 19:44:10 +0100

    added dbt_utils package

---
+++ sde_dbt_tutorial/macros/cents_to_dollars.sql
@@ -0,0 +1,4 @@
+
+{% macro cents_to_dollars(column_name, precision=2) %}
+    ({{ column_name }} / 100)::numeric(16, {{ precision }})
+{% endmacro %}
---
+++ sde_dbt_tutorial/models/marts/core/int_orders__pivoted.sql
@@ -0,0 +1,19 @@
+{%- set order_statuses = ['delivered', 'invoiced', 'shipped', 'processing', 'canceled', 'unavailable'] -%}
+
+with orders as (
+    select * from {{ ref('stg_eltool__orders') }}
+),
+
+pivoted as (
+    select 
+        customer_id,
+        {%- for order_status in order_statuses -%}
+            sum(case when order_status = '{{ order_status }}' then 1 else 0 end) as {{ order_status }}_count
+            {% if not loop.last %}, {%- endif %}
+        {% endfor -%}
+    from orders
+    group by customer_id
+)
+
+
+select * from pivoted
