abf00b43f1e48bd2341f6576a00c6284149eab60
JosephMachado <joseph.machado.professional@gmail.com>
2024-01-16 09:58:52 -0500

    add checks for models without ref

--- check_orphans.py
+++ check_orphans.py
@@ -8,6 +8,7 @@ with open(manifest_file_path, "r") as file:
     manifest_data = json.load(file)
 
 dbt_tests = [k for k in manifest_data["nodes"].keys() if k.startswith("test")]
+dbt_models = [k for k in manifest_data["nodes"].keys() if k.startswith("model")]
 
 for dbt_test in dbt_tests:
     if (
@@ -23,5 +24,19 @@ for dbt_test in dbt_tests:
             f"""
             ####################################################### \n
             The test {dbt_test} is an orphaned test (no model to run this test on) please remove it! \n
+            ####################################################### \n"""
+        )
+
+for dbt_model in dbt_models:
+    dbt_model_node = manifest_data.get("nodes", {}).get(dbt_model, {})
+    if (
+        len(dbt_model_node.get("refs", [])) == 0
+        and len(dbt_model_node.get("sources", [])) == 0
+        and dbt_model_node.get("fqn", ["non-existent"])[0] != "elementary"
+    ):
+        logging.warning(
+            f"""
+            ####################################################### \n
+            The model {'.'.join(dbt_model_node.get('fqn', []))} does not have any sources or ref function, please check it! \n
             ####################################################### \n"""
         )
