1b903c4e445a397fe2d1670d1e3edd1b7bae5d4a
JosephKevinMachado <joseph.machado.professional@gmail.com>
2025-06-04 21:10:54 -0400

    2025-06-04-21-10-54

--- models/staging/stg_eltool__customers.sql
+++ models/staging/stg_eltool__customers.sql
@@ -9,8 +9,8 @@ renamed as (
         zipcode,
         city,
         state_code,
-        datetime_created::TIMESTAMP as datetime_created,
-        datetime_updated::TIMESTAMP as datetime_updated,
+        datetime_created::timestamp as datetime_created,
+        datetime_updated::timestamp as datetime_updated,
         dbt_valid_from,
         dbt_valid_to
     from source
--- models/staging/stg_eltool__orders.sql
+++ models/staging/stg_eltool__orders.sql
@@ -8,11 +8,13 @@ renamed as (
         order_id,
         customer_id,
         order_status,
-        order_purchase_timestamp::TIMESTAMP AS order_purchase_timestamp,
-        order_approved_at::TIMESTAMP AS order_approved_at,
-        order_delivered_carrier_date::TIMESTAMP AS order_delivered_carrier_date,
-        order_delivered_customer_date::TIMESTAMP AS order_delivered_customer_date,
-        order_estimated_delivery_date::TIMESTAMP AS order_estimated_delivery_date
+        order_purchase_timestamp::timestamp as order_purchase_timestamp,
+        order_approved_at::timestamp as order_approved_at,
+        order_delivered_carrier_date::timestamp as order_delivered_carrier_date,
+        order_delivered_customer_date::timestamp
+            as order_delivered_customer_date,
+        order_estimated_delivery_date::timestamp
+            as order_estimated_delivery_date
     from source
 )
 
